43987e651e2d051a7d6252e4611b63bbf744ff09
armond <mondoloves@icloud.com>
2025-07-10 15:44:47 -0700

    inital

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
